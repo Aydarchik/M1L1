7b3caf2087e45a962ceb130c77a84cb897c35d01
Aydarchik <126374145+Aydarchik@users.noreply.github.com>
2024-02-11 13:11:50 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,34 @@
 from bot_logic import gen_pass
 from rnumber import r_number
 import discord
+from discord.ext import commands
+import os
 import random
 
-
-# Переменная intents - хранит привилегии бота
 intents = discord.Intents.default()
-# Включаем привелегию на чтение сообщений
 intents.message_content = True
-# Создаем бота в переменной client и передаем все привелегии
-client = discord.Client(intents=intents)
 
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-
-
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\\U0001f642")
-    elif message.content.startswith('!пароль'):
-        await message.channel.send(gen_pass(8))
-    elif message.content.startswith('!номер любой'):
-        await message.channel.send(r_number(1))
-    elif message.content.startswith('!СТОП'):
-        await message.channel.send('СТООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООП')
-    else:
-        await message.channel.send(message.content)
-        
-@bot.command()    
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
 
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+@bot.command()
+async def mem(ctx):
+    images = os.listdir('images')
+    img_name = random.choice(images)
+    with open(f'images/{img_name}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
 
-
-client.run('')
+bot.run("")
